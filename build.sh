8af492607487d59126321a8ae82bfeb89701674f
htchepannou <htchepannou@expedia.com>
2021-08-17 19:15:28 -0400

    add get-application

--- build.sh
+++ build.sh
@@ -11,6 +11,6 @@ mkdir target
 for file in $(find . -type f -iname '*_api.yaml'); do
   filename=$(basename $file suffix)
 
-  echo ">>> Validating $file"
   swagger-cli validate $file
 done
+echo "DONE"
